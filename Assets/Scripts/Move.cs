0a030d01b8a424eeb6be04b265c4e78d73f27342
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-01-22 17:14:20 +0700

    auto commit - test on rider -

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] private Transform targetPosition;
+
+    [SerializeField] private float speedMove = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3.MoveTowards(this.transform.position, targetPosition.position, speedMove);
     }
 }
