f280b1809c087a1827a6b6255dabecd925099c1d
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-02-24 09:00:31 +0700

    add file bornEnemy

---
+++ Assets/Scripts/BornEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BornEnemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] posBorn;
+
+    [SerializeField] private GameObject[] pre_Enemy;
+
+    private GameObject pre_Select;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
