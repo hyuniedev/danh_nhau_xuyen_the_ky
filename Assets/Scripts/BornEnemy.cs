60149a2014d6f75405257f762d05363b5df8eea3
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-02-26 17:52:29 +0700

    auto commit 26.2

--- Assets/Scripts/BornEnemy.cs
+++ Assets/Scripts/BornEnemy.cs
@@ -1,17 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 public class BornEnemy : MonoBehaviour
+
 {
     [SerializeField] private Transform[] posBorn;
 
     [SerializeField] private GameObject[] pre_Enemy;
+    
+    [SerializeField] private Transform parent_Enemy;
+    private float timeBorn = 2;
 
-    private GameObject pre_Select;
-
-    private void randomSelectedEnemy()
+    private void Start()
     {
-        pre_Select = pre_Enemy[Random.Range(0, pre_Enemy.Length)];
+        InvokeRepeating(nameof(Born),3.0f,timeBorn);
+    }
+
+    private void Born()
+    {
+        timeBorn = Random.Range(3, 10);
+        Invoke(nameof(InstanRandomPosition),0);
+    }
+
+    private GameObject randomSelectedEnemy()
+    {
+        return pre_Enemy[Random.Range(0, pre_Enemy.Length)];
+    }
+
+    private Transform randomTransformBornEnemy()
+    {
+        return posBorn[Random.Range(0, posBorn.Length)];
+    }
+
+    private void InstanRandomPosition()
+    {
+        Instantiate(randomSelectedEnemy(), randomTransformBornEnemy().position, transform.rotation, parent_Enemy);
     }
 }
